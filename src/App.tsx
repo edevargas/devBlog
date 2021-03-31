4260bf003a8b515116e8832c6ac2b61e634abcbc
Elías De Vargas <sailedvc@gmail.com>
2021-03-30 20:34:17 -0500

    [FEAT] basic React + Typescript + ESLint config

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const App: React.FC = () => {
+  return (
+    <div>
+            I am working
+    </div>
+  )
+}
+export default App
---
+++ src/styles/devBlogTheme.ts
@@ -0,0 +1,12 @@
+import { DefaultTheme } from 'styled-components'
+
+const devBlogTheme: DefaultTheme = {
+  borderRadius: '5px',
+
+  colors: {
+    main: 'cyan',
+    secondary: 'magenta',
+  },
+}
+
+export { devBlogTheme }
